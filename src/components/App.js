b57bfb264e584a13f0f0a130878e2a427963bcad
Sam Duemler <samduemler@gmail.com>
2019-08-12 20:44:34 -0500

    Now grabbing photos from Kaitlyn's album of website photos.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 
-const url = 'https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=d9bcbdfdbee02fb2af1f8e5dbb2bc321&user_id=160810395@N08&format=json&nojsoncallback=1';
+const baseURL = 'https://www.flickr.com/services/rest/';
+const flickrMethod = '?method=flickr.photosets.getPhotos';
+const api_key = `api_key=${process.env.REACT_APP_FLICKR_API_KEY}`;
+const user_id = 'user_id=160810395@N08&';
+const photoset_id = 'photoset_id=72157710272039597';
+const urlEnd = 'format=json&nojsoncallback=1';
+const url = baseURL + flickrMethod + '&' + api_key + '&' + user_id + '&' + photoset_id + '&' + urlEnd;
 
 class App extends React.Component {
     state = { images: [], photoUrls: []};
@@ -14,17 +20,16 @@ class App extends React.Component {
 
     getPhotos = async () => {
         const response = await axios.get(url);
-        this.setState( {images: response.data.photos.photo});
-        let urlString = `https://farm${this.state.images[0].farm}.staticflickr.com/`;
-        urlString += `${this.state.images[0].server}/${this.state.images[0].id}_${this.state.images[0].secret}.jpg`;
-        this.setState({photoUrls: urlString});
-        console.log(this.state.photoUrls);
+        //console.log(response);
+        this.setState( {images: response.data.photoset.photo});
+        this.setState({photoUrls: this.getPhotoURL()});
+        //console.log(this.state.photoUrls);
     };
 
     getPhotoURL = () => {
         let urlString = `https://farm${this.state.images[0].farm}.staticflickr.com/`;
         urlString += `${this.state.images[0].server}/${this.state.images[0].id}_${this.state.images[0].secret}.jpg`;
-        console.log(urlString);
+        //console.log(urlString);
         return urlString;
     };
 
